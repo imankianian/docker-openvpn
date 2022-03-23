42410e06933c4a8f924ccac880ea05d90c09d3ef
Iman Kianian <iman.kianian98@gmail.com>
2022-03-24 01:23:22 +0430

    Merge echo commands

--- setup-openvpn.sh
+++ setup-openvpn.sh
@@ -22,49 +22,49 @@ OUTPUT_DIR=$CERTIFICATES_DIR/files
 
 make_certificates() {
 
-    mkdir -p $CERTIFICATES_DIR/keys
-    mkdir -p $CERTIFICATES_DIR/files
-    echo "client" > $CERTIFICATES_DIR/base.conf
-    echo "remote $SERVER_IP 1194" >> $CERTIFICATES_DIR/base.conf
-    echo "proto udp" >> $CERTIFICATES_DIR/base.conf
-    echo "dev tun" >> $CERTIFICATES_DIR/base.conf
-    echo "nobind" >> $CERTIFICATES_DIR/base.conf
-    echo "persist-key" >> $CERTIFICATES_DIR/base.conf
-    echo "persist-tun" >> $CERTIFICATES_DIR/base.conf
-    echo "remote-cert-tls server" >> $CERTIFICATES_DIR/base.conf
-    echo "tls-auth ta.key 1" >> $CERTIFICATES_DIR/base.conf
-    echo "user nobody" >> $CERTIFICATES_DIR/base.conf
-    echo "group nogroup" >> $CERTIFICATES_DIR/base.conf
-    echo "ca ca.crt" >> $CERTIFICATES_DIR/base.conf
-    echo "cert client.crt" >> $CERTIFICATES_DIR/base.conf
-    echo "key client.key" >> $CERTIFICATES_DIR/base.conf
-    echo "cipher AES-256-CBC" >> $CERTIFICATES_DIR/base.conf
-    echo "auth SHA256" >> $CERTIFICATES_DIR/base.conf
-    echo "key-direction 1" >> $CERTIFICATES_DIR/base.conf
-    echo "verb 3" >> $CERTIFICATES_DIR/base.conf
+mkdir -p $CERTIFICATES_DIR/keys
+mkdir -p $CERTIFICATES_DIR/files
 
+echo "client
+remote $SERVER_IP 1194
+proto udp
+dev tun
+nobind
+persist-key
+persist-tun
+remote-cert-tls server
+tls-auth ta.key 1
+user nobody
+group nogroup
+ca ca.crt
+cert client.crt
+key client.key
+cipher AES-256-CBC
+auth SHA256
+key-direction 1
+verb 3" > $CERTIFICATES_DIR/base.conf
 
-    for ((i=0; i<$NUM; i++)); do
+for ((i=0; i<$NUM; i++)); do
 
-        $EASYRSA_DIR/easyrsa gen-req client$i nopass
-	$EASYRSA_DIR/easyrsa sign-req client client$i
-	cp /pki/issued/client$i.crt $CERTIFICATES_DIR/keys
-	cp /pki/private/client$i.key $CERTIFICATES_DIR/keys
-	cp $SERVER_DIR/ta.key $CERTIFICATES_DIR/keys
-	cp $SERVER_DIR/ca.crt $CERTIFICATES_DIR/keys
-	cat ${BASE_CONFIG} \
-	    <(echo -e "<ca>") \
-  	    ${KEY_DIR}/ca.crt \
-	    <(echo -e "</ca>\n<cert>") \
-	    ${KEY_DIR}/client$i.crt \
-	    <(echo -e "</cert>\n<key>") \
-	    ${KEY_DIR}/client$i.key \
-	    <(echo -e "</key>\n<tls-auth>") \
-	    ${KEY_DIR}/ta.key \
-	    <(echo -e "</tls-auth>") \
-	    > ${OUTPUT_DIR}/client$i.ovpn	
+    $EASYRSA_DIR/easyrsa gen-req client$i nopass
+    $EASYRSA_DIR/easyrsa sign-req client client$i
+    cp /pki/issued/client$i.crt $CERTIFICATES_DIR/keys
+    cp /pki/private/client$i.key $CERTIFICATES_DIR/keys
+    cp $SERVER_DIR/ta.key $CERTIFICATES_DIR/keys
+    cp $SERVER_DIR/ca.crt $CERTIFICATES_DIR/keys
+    cat ${BASE_CONFIG} \
+    <(echo -e "<ca>") \
+    ${KEY_DIR}/ca.crt \
+    <(echo -e "</ca>\n<cert>") \
+    ${KEY_DIR}/client$i.crt \
+    <(echo -e "</cert>\n<key>") \
+    ${KEY_DIR}/client$i.key \
+    <(echo -e "</key>\n<tls-auth>") \
+    ${KEY_DIR}/ta.key \
+    <(echo -e "</tls-auth>") \
+    > ${OUTPUT_DIR}/client$i.ovpn	
 
-    done    
+done    
 }
 
 # Setup container & install necessary packages
@@ -95,29 +95,29 @@ cp /pki/dh.pem $SERVER_DIR
 openvpn --genkey --secret $SERVER_DIR/ta.key
 
 # Configure OpenVPN server
-echo "tls-auth $SERVER_DIR/ta.key 0" > $SERVER_DIR/server.conf
-echo "cipher AES-256-CBC" >> $SERVER_DIR/server.conf
-echo "auth SHA256" >> $SERVER_DIR/server.conf
-echo "dh $SERVER_DIR/dh.pem" >> $SERVER_DIR/server.conf
-echo "user nobody" >> $SERVER_DIR/server.conf
-echo "group nogroup" >> $SERVER_DIR/server.conf
-echo 'push "redirect-gateway def1 bypass-dhcp"' >> $SERVER_DIR/server.conf
-echo 'push "dhcp-option DNS 208.67.222.222"' >> $SERVER_DIR/server.conf
-echo 'push "dhcp-option DNS 208.67.220.220"' >> $SERVER_DIR/server.conf
-echo "port 1194" >> $SERVER_DIR/server.conf
-echo "proto udp" >> $SERVER_DIR/server.conf
-echo "explicit-exit-notify 1" >> $SERVER_DIR/server.conf
-echo "dev tun" >> $SERVER_DIR/server.conf
-echo "ca $SERVER_DIR/ca.crt" >> $SERVER_DIR/server.conf
-echo "cert $SERVER_DIR/server.crt" >> $SERVER_DIR/server.conf
-echo "key $SERVER_DIR//server.key" >> $SERVER_DIR/server.conf
-echo "server 10.8.0.0 255.255.255.0" >> $SERVER_DIR/server.conf
-echo "ifconfig-pool-persist /var/log/openvpn/ipp.txt" >> $SERVER_DIR/server.conf
-echo "keepalive 10 120" >> $SERVER_DIR/server.conf
-echo "persist-key" >> $SERVER_DIR/server.conf
-echo "persist-tun" >> $SERVER_DIR/server.conf
-echo "status /var/log/openvpn/openvpn-status.log" >> $SERVER_DIR/server.conf
-echo "verb 3" >> $SERVER_DIR/server.conf
+echo "tls-auth $SERVER_DIR/ta.key 0
+cipher AES-256-CBC
+auth SHA256
+dh $SERVER_DIR/dh.pem
+user nobody
+group nogroup
+push \"redirect-gateway def1 bypass-dhcp\"
+push \"dhcp-option DNS 208.67.222.222\"
+push \"dhcp-option DNS 208.67.220.220\"
+port 1194
+proto udp
+explicit-exit-notify 1
+dev tun
+ca $SERVER_DIR/ca.crt
+cert $SERVER_DIR/server.crt
+key $SERVER_DIR/server.key
+server 10.8.0.0 255.255.255.0
+ifconfig-pool-persist /var/log/openvpn/ipp.txt
+keepalive 10 120
+persist-key
+persist-tun
+status /var/log/openvpn/openvpn-status.log
+verb 3" >> $SERVER_DIR/server.conf
 
 # Adjust network settings
 sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/" /etc/sysctl.conf
