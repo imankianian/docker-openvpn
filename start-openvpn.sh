55458f67a19df0853ded3126241b5ca6a70dda46
Iman Kianian <iman.kianian98@gmail.com>
2022-07-12 10:53:16 +0430

    Add iptables and nodes for openvpn to work properly on restarts

--- start-openvpn.sh
+++ start-openvpn.sh
@@ -9,7 +9,31 @@ then
 
 	if [[ "$VALUE" == "OK" ]]
 	then
-	openvpn --config $SERVER_DIR/server.conf
+
+		# Since the node disappears when container stops, we need to recreate it on restarts
+
+		mkdir -p /dev/net
+		if [[ ! -c /dev/net/tun ]]
+		then
+			mknod /dev/net/tun c 10 200
+		fi
+
+		# set iptables rules again since they've been removed when container stopped
+
+		INTERFACE=$(ip route list default | cut -f 5 -d " ")
+		SERVER_PORT=$(grep "port" /etc/openvpn/server/server.conf | cut -d' ' -f2)
+
+		iptables -A INPUT -i $INTERFACE -m state --state NEW -p udp --dport $SERVER_PORT -j ACCEPT
+		iptables -A INPUT -i tun0 -j ACCEPT
+		iptables -A FORWARD -i tun0 -j ACCEPT
+		iptables -A FORWARD -i tun0 -o $INTERFACE -m state --state RELATED,ESTABLISHED -j ACCEPT
+		iptables -A FORWARD -i $INTERFACE -o tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+		iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o $INTERFACE -j MASQUERADE
+		iptables -A OUTPUT -o tun0 -j ACCEPT
+
+		# Start the service
+
+		openvpn --config $SERVER_DIR/server.conf
 	fi
 else
 	
